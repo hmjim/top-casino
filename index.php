8df7a2c6dfff7ef90bad1d7b3bcad98ff497be7c
Max <hmjim@mail.ru>
2022-02-01 23:41:16 +0700

    01.02

--- index.php
+++ index.php
@@ -78,7 +78,9 @@ curl_close( $ch );
 $bodytag = str_replace( "((?!topcasinozz\.net/b)\w+(?:\.\w+)+", "https://tops-cazino.azurewebsites.net", $contents );
 $result  = preg_replace( '~' . $new_url . '~m', "tops-cazino.azurewebsites.net", $contents );
 preg_match_all( "/(https:\/\/topcasinozz.net).*\.(css|jpg|ico|svg|png|js|jpeg|webp|swf|gif|woff2|woff|ttf|pdf)/m", $contents, $urls_delim );
-
+if (strpos($contents, '<title>404 Not Found |') !== false) {
+	header("HTTP/1.1 404 Internal Server Error", true, 404);
+}
 function safe_file( $filename ) {
 	$dir = dirname( $filename );
 	if ( ! file_exists( __DIR__ . $dir ) ) {
